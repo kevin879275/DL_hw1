79603fa7abb0324a10c749d59608570458f95958
kevin879275 <gevin879275@gmail.com>
2020-10-16 09:29:24 +0800

    update file

---
+++ hw1report_109062598.py
@@ -0,0 +1,31 @@
+import numpy as np
+import os
+
+
+def DataLoader():
+    pass
+
+
+def Cross_Entropy(y, y_predict):
+    pass
+
+
+def Softmax():
+  # e^z(index)/sum of(all e^z(index))
+    pass
+
+
+def ReLU(value):
+    return 0 if (value == 0) else value
+
+
+def Backward():
+    pass
+
+
+def Dense():
+    pass
+
+
+if __name__ == '__main__':
+    DataLoader()
